cab20e74f2bdfd6b4c6d626d784258f6a24baa23
XanderL2 <alexanderllave2021@gmail.com>
2023-11-03 11:55:15 +0100

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear(); 
+        /*Ejercicio: "Suma de elementos pares"
+        Escribe un programa en C# que solicite al usuario ingresar una serie de números enteros. El programa debe almacenar estos números en una lista. Luego, el programa debe sumar todos los números pares en la lista y mostrar el resultado.*/
+
+        //Declaracion de las variables a usar 1er commit: 
+        int lenght;
+        int[]matriz_nums;  
+        Console.WriteLine("Ingrese la cantidad de numeros a ingresar: ");  lenght = int.Parse(Console.ReadLine());
+        matriz_nums = new int[lenght];
+
+        
+        
+
+
+
+
+
+
+
+    }
+}
